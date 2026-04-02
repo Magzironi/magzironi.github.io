471102092d4d3e24c8db11f49b6ac0c6353f07ee
Magzironi <magsannsmith@gmail.com>
2026-04-02 15:59:50 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
-    console.log("Pixi script running");
+console.log("JS is running");
+
+// Create app
+const app = new PIXI.Application({
+    background: '#1099bb',
+    resizeTo: window
+});
+
+// Add canvas to page
+const container = document.getElementById('pixi-canvas');
+
+if (!container) {
+    console.error("No container found!");
+} else {
+    container.appendChild(app.view);
+}
+
+// Draw rectangle
+const graphics = new PIXI.Graphics();
+graphics.beginFill(0xFFFF00);
+graphics.drawRect(0, 0, 200, 200);
+graphics.endFill();
+
+graphics.x = 100;
+graphics.y = 100;
+
+app.stage.addChild(graphics);
+console.log("Pixi script running");
 
     // Create the app
     const app = new PIXI.Application();
